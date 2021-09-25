4e7bc270fcf98517393f19dc0ea1182fdb772d8d
kostyashelest <skostya88@gmail.com>
2021-09-25 14:03:30 +0300

    Добавил обнуление счетчика

--- commands/StatisticsController.php
+++ commands/StatisticsController.php
@@ -8,7 +8,14 @@ use app\models\Input;
 
 class StatisticsController extends Controller
 {
-    const NUMBER_EMPTY_VALUES = 7; // количество дней через которое деактивируем input_id
+    /**
+     * const NUMBER_EMPTY_VALUES - количество дней через которое деактивируем input_id
+     * Бывают дни когда результат парсинга = 0, то есть отсутствуют вакансии по запросу или упоминания скилла.
+     * В этом случае считаем такие дни в столбце number_empty_values, таблица input.
+     * Если в счетчике собирает количество >= указанное в константе NUMBER_EMPTY_VALUES, то input_id деактивируется.
+     * Учитываются только дни подряд. При положительном парсинге, счетчик сбрасывается к 0.
+     */
+    const NUMBER_EMPTY_VALUES = 7;
 
     /**
      * Производит расчет и запись дневного изменений результатов парсинга.
@@ -33,7 +40,7 @@ class StatisticsController extends Controller
                 continue;
             }
 
-            // получаем результат сегодняшний результат парсинга для перебираемого input_id
+            // получаем сегодняшний результат парсинга для перебираемого input_id
             $todayQuantity = Results::find()
                 ->asArray()
                 ->select(['id', 'quantity'])
@@ -43,7 +50,7 @@ class StatisticsController extends Controller
             if (empty($todayQuantity) || $todayQuantity['quantity'] == '0') {
                 $mes = 'Пустое значение quantity: input_id ' . $dataInput[0]['id'];
 
-                // если результат парсинга за сегодня - пусть, то добавляем в счетчик 1 и отправляем уведомление в тг
+                // если результат парсинга за сегодня - пуст, то добавляем в счетчик 1 и отправляем уведомление в тг
                 try {
                     $model = Input::findOne($input[$i]['id']);
                     $model->number_empty_values = $dataInput[0]['number_empty_values'] + 1;
@@ -72,28 +79,48 @@ class StatisticsController extends Controller
                 continue;
             }
 
+            // если результат парсинга за сегодня не пуст и у input_id счетчик > 1, то делаем обнуление счетчика
+            if ($todayQuantity['quantity'] > '0' && $dataInput[0]['number_empty_values'] >= 1){
+                try {
+                    $model = Input::findOne($input[$i]['id']);
+                    $model->number_empty_values = 0;
+                    $model->save();
+                    $sendToTelegram = fopen('https://api.telegram.org/bot1908284524:AAGMSVUc06Z2Iqsay5p-4m8lhfF8tacmH7U/sendMessage?chat_id=347810962&parse_mode=html&text=Обнуление счетчика для input_id ' . $input[$i]['id'], "r");
+                    fclose($sendToTelegram);
+                } catch (\Exception $e) {
+                    $sendToTelegram = fopen('https://api.telegram.org/bot1908284524:AAGMSVUc06Z2Iqsay5p-4m8lhfF8tacmH7U/sendMessage?chat_id=347810962&parse_mode=html&text=Ошибка обнуления счетчика для input_id ' . $input[$i]['id'], "r");
+                    fclose($sendToTelegram);
+                }
+            }
+
+            // получаем вчерашний результат парсинга для перебираемого input_id
             $yesterdayQuantity = Results::find()
                 ->asArray()
                 ->select(['id', 'quantity'])
                 ->where(['date' => $yesterdayDate, 'input_id' => $input[$i]['id']])
                 ->one();
 
+            // отправляем уведомление если вчерашний результат пуст и переходим к следующему input_id
             if (empty($yesterdayQuantity) || $yesterdayQuantity['quantity'] == '0') {
                 $sendToTelegram = fopen('https://api.telegram.org/bot1908284524:AAGMSVUc06Z2Iqsay5p-4m8lhfF8tacmH7U/sendMessage?chat_id=347810962&parse_mode=html&text=ошибка консольной команды actionToday. пустое значение $yesterdayQuantity, ' . $input[$i]['id'], "r");
                 fclose($sendToTelegram);
                 continue;
             }
 
+            // сортируем результат парсинга за сегодня и вчера (для дальнейшего расчета)
             $data = $this->determiningOrderValues($yesterdayQuantity['quantity'], $todayQuantity['quantity']);
 
+            // расчитываем числовую и процентную разницу между сегодняшним и вчерашним результатом
             $calculation = $this->calculationDifference($data, $todayQuantity['quantity'], $yesterdayQuantity['quantity']);
 
+            // отправляем уведомление если расчет не удался и переходим к следующему input_id
             if (empty($calculation)) {
                 $sendToTelegram = fopen('https://api.telegram.org/bot1908284524:AAGMSVUc06Z2Iqsay5p-4m8lhfF8tacmH7U/sendMessage?chat_id=347810962&parse_mode=html&text=ошибка консольной команды actionToday. пустое значение $calculation', "r");
                 fclose($sendToTelegram);
                 continue;
             }
 
+            // сохраняем результат дневного изменения в столбец change_per_day, таблица results
             try {
                 $model = Results::findOne($todayQuantity['id']);
                 $model->change_per_day = json_encode($calculation);
