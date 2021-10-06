939b9a002baf0bfa40171adfcc7979ad2f0ec99e
kostyashelest <skostya88@gmail.com>
2021-10-07 01:27:13 +0300

    Корректное логирование в отдельный файл

--- commands/ParseController.php
+++ commands/ParseController.php
@@ -21,36 +21,43 @@ class ParseController extends Controller
      */
     public function actionSkill()
     {
-        $skill = Input::find()->select(['id', 'query'])->where(['type' => 'skill', 'status' => 1])->distinct()->all();
+        $skill = Input::find()
+            ->select(['id', 'query'])
+            ->where(['type' => 'skill', 'status' => 1])
+            ->distinct()
+            ->all();
         $date = date('Y-m-d');
-        echo 'Количество записей для выборки: ' . count($skill) . "\n";
+        $this->stdout("*************** start actionSkill (" . $date . ") *************** \n");
+        $this->stdout('Всего записей для выборки: ' . count($skill) . "\n");
         for ($i = 0; $i < count($skill); $i++) {
-            echo $skill[$i]['query'] . "\n";
+            $this->stdout("*************** #" . $i . " *************** \n");
+            $this->stdout("Скилл: " . $skill[$i]['query'] . "\n");
             try {
                 $link = 'https://hh.ru/search/vacancy?clusters=true&area=1&specialization=1&enable_snippets=true&salary=&st=searchVacancy&text=' . $skill[$i]['query'] . '&from=suggest_post';
                 $file = file_get_contents($link);
             } catch (\Exception $e) {
-                echo 'Данные не получены (ошибка парсинга): ' . $skill[$i]['query'];
+                $this->stdout("Данные не получены (ошибка парсинга): " . $skill[$i]['query'] . "\n");
                 $sendToTelegram = fopen('https://api.telegram.org/bot1908284524:AAGMSVUc06Z2Iqsay5p-4m8lhfF8tacmH7U/sendMessage?chat_id=347810962&parse_mode=html&text=ошибка парсинга: ' . $skill[$i]['query'], "r");
                 fclose($sendToTelegram);
                 continue;
             }
             preg_match_all('/"totalResults": (.*), "enableNovaFilters"/', $file, $result);
-            echo 'Количество упоминаний: ' . $result[1][0] . "\n";
-            echo 'Дата парсинга: ' . $date . "\n";
+            $this->stdout("Количество упоминаний: " . $result[1][0] . "\n");
+            $this->stdout("Дата парсинга: " . $date . "\n");
             $model = new Results();
             $model->input_id = $skill[$i]['id'];
             $model->date = $date;
             $model->quantity = $result[1][0];
             if (!$model->save()) {
-                echo 'Данные не сохранены: ' . $skill[$i]['query'];
+                $this->stdout("Данные не сохранены: " . $skill[$i]['query'] . "\n");
                 $sendToTelegram = fopen('https://api.telegram.org/bot1908284524:AAGMSVUc06Z2Iqsay5p-4m8lhfF8tacmH7U/sendMessage?chat_id=347810962&parse_mode=html&text=ошибка парсинга: ' . $skill[$i]['query'], "r");
                 fclose($sendToTelegram);
             }
+            $this->stdout("*************** *************** \n");
             $sec = rand(25, 45);
             sleep($sec);
         }
-
+        $this->stdout("*************** end actionSkill (" . $date . ") *************** \n");
         return ExitCode::OK;
     }
 
@@ -60,38 +67,43 @@ class ParseController extends Controller
      */
     public function actionVacancies()
     {
-        $vacancies = Input::find()->select(['id', 'query', 'description'])->where(['type' => 'vacancies', 'status' => 1])->distinct()->all();
+        $vacancies = Input::find()
+            ->select(['id', 'query', 'description'])
+            ->where(['type' => 'vacancies', 'status' => 1])
+            ->distinct()
+            ->all();
         $date = date('Y-m-d');
-        echo 'Количество записей для выборки: ' . count($vacancies) . "\n";
+        $this->stdout("*************** start actionVacancies (" . $date . ") *************** \n");
+        $this->stdout("Всего записей для выборки: " . count($vacancies) . "\n");
         for ($i = 0; $i < count($vacancies); $i++) {
-            $this->stdout("*************** *************** \n");
+            $this->stdout("*************** #" . $i . " *************** \n");
             $this->stdout("Специализация: " . $vacancies[$i]['description'] . "\n");
             $link = $vacancies[$i]['query'];
             try {
                 $file = file_get_contents($link);
             } catch (\Exception $e) {
-                echo 'Данные не получены (ошибка парсинга): ' . $vacancies[$i]['query'];
-                $sendToTelegram = fopen('https://api.telegram.org/bot1908284524:AAGMSVUc06Z2Iqsay5p-4m8lhfF8tacmH7U/sendMessage?chat_id=347810962&parse_mode=html&text=ошибка парсинга: ' . $skill[$i]['query'], "r");
+                $this->stdout("Данные не получены (ошибка парсинга): " . $vacancies[$i]['query'] . "\n");
+                $sendToTelegram = fopen('https://api.telegram.org/bot1908284524:AAGMSVUc06Z2Iqsay5p-4m8lhfF8tacmH7U/sendMessage?chat_id=347810962&parse_mode=html&text=ошибка парсинга: ' . $vacancies[$i]['query'], "r");
                 fclose($sendToTelegram);
                 continue;
             }
             preg_match_all('/"totalResults": (.*), "enableNovaFilters"/', $file, $result);
             $this->stdout("Количество вакансий: " . $result[1][0] . "\n");
             $this->stdout("Дата парсинга: " . $date . "\n");
-            $this->stdout("*************** *************** \n");
             $model = new Results();
             $model->input_id = $vacancies[$i]['id'];
             $model->date = $date;
             $model->quantity = $result[1][0];
             if (!$model->save()) {
-                echo 'Данные не сохранены: ' . $vacancies[$i]['query'];
-                $sendToTelegram = fopen('https://api.telegram.org/bot1908284524:AAGMSVUc06Z2Iqsay5p-4m8lhfF8tacmH7U/sendMessage?chat_id=347810962&parse_mode=html&text=ошибка парсинга: ' . $skill[$i]['query'], "r");
+                $this->stdout("Данные не сохранены: " . $vacancies[$i]['query'] . "\n");
+                $sendToTelegram = fopen('https://api.telegram.org/bot1908284524:AAGMSVUc06Z2Iqsay5p-4m8lhfF8tacmH7U/sendMessage?chat_id=347810962&parse_mode=html&text=ошибка парсинга: ' . $vacancies[$i]['query'], "r");
                 fclose($sendToTelegram);
             }
+            $this->stdout("*************** *************** \n");
             $sec = rand(25, 45);
             sleep($sec);
         }
-
+        $this->stdout("*************** end actionVacancies (" . $date . ") *************** \n");
         return ExitCode::OK;
     }
 }
